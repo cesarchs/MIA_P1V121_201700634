345f1b5681e42b3f464632d24a7812b70e8a81bb
cesar chamale <58505340+cesarchs@users.noreply.github.com>
2021-06-14 11:02:07 -0600

    metodos 2.1

--- MIA_Proyecto1/mkdisk.cpp
+++ MIA_Proyecto1/mkdisk.cpp
@@ -1,4 +1,8 @@
 #include "mkdisk.h"
+//#include "estructuras.h"
+#include <stdio.h>
+#include <string.h>
+using namespace std;
 
 mkdisk::mkdisk()
 {
@@ -9,5 +13,43 @@ void mkdisk::mostrardatos(mkdisk *disco){
     printf("\n\n-------------DATOS-------------\n\n");
 
     printf("EL tamanio es %d \n",disco->size);
+
+}
+
+void mkdisk::crearFichero(mkdisk *disco){
+    mbr prueba;
+    FILE *arch;
+    arch=fopen(disco->path.c_str(),"wb");
+    if (arch==NULL)
+        exit(1);
+    //simulo un KB
+    char buffer[1024];
+
+    //si son Kb
+    if (disco->unit=="k"){
+        //-------ASIGNO TAMAÑO A MBR, RECORDAR QUE ESTE LO PIDE EN BYTES POR LO que multiplico el size por 1024
+        prueba.mbr_tamano=disco->size*1024;
+
+        for(int i = 0 ; i < 1024 ; i++)
+            buffer[i]='\0';
+
+        for(int i = 0 ; i < (disco->size) ; i++)
+            fwrite(&buffer,1024,1,arch);
+
+
+        fclose(arch);
+    }
+
+    //Si son megas
+    else if(disco->unit=="m" || disco->unit.empty()==1){
+        prueba.mbr_tamano=disco->size*1024*1024;//LO DOY EN BYTES
+        for(int i = 0 ; i < 1024 ; i++)
+            buffer[i]='\0';
+        for(int i = 0 ; i < (disco->size*1024) ; i++)//para mb multiplicar por 1024
+            fwrite(&buffer,1024,1,arch);
+        fclose(arch);
+    }
+
+
 
 }
--- MIA_Proyecto1/mkdisk.h
+++ MIA_Proyecto1/mkdisk.h
@@ -10,10 +10,17 @@ class mkdisk
 public:
     mkdisk();
     //ACA VAN LAS DEFINICIONES ATRIBUTOS Y METODOS
+
     int size;
+    string unit;
+    string path;
+    string ajuste;
+
 
     void mostrardatos (mkdisk *disco);// se le pasa un puntero, se trabaja por referencia, es decir la direccion del objeto
     void ejecutar (mkdisk *disco);// este para que aca haga todos los procesos de creacion de lo que pidan para este metodo
+    void crearFichero(mkdisk * disco);//declaramos el metodo para crear ficheros
+
 };
 
 #endif // MKDISK_H
